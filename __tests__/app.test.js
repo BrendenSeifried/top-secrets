7da8ae444f3a5775ba1ccd8a7cef1f5a0b10ae14
BrendenSeifried <brendenseifried@gmail.com>
2022-06-21 17:11:10 -0700

    userservices created in services test to create a user now pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const testUser = {
   email: '1@1.com',
@@ -13,9 +14,7 @@ describe('top-secret test bed', () => {
     return setup(pool);
   });
   it('Test to Create a new user', async () => {
-    const resp = await (
-      await request(app).post('/api/v1/users')
-    ).setEncoding(testUser);
+    const resp = await request(app).post('/api/v1/users').send(testUser);
     const { email } = testUser;
 
     expect(resp.body).toEqual({
